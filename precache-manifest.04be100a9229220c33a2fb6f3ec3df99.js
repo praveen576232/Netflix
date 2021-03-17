70ce26967dce84b0826f0a771d5ea594d612c0fd
praveend <praveend2622@gmail.com>
2021-03-17 14:32:33 +0530

    Updates

---
+++ precache-manifest.04be100a9229220c33a2fb6f3ec3df99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd065dc18a4be7a0d7758eac3233aca",
+    "url": "/Netflix/index.html"
+  },
+  {
+    "revision": "6c63694392b293e04f53",
+    "url": "/Netflix/static/css/main.b9161fee.chunk.css"
+  },
+  {
+    "revision": "4366ca8999c47fd26233",
+    "url": "/Netflix/static/js/2.2b52b78c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix/static/js/2.2b52b78c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c63694392b293e04f53",
+    "url": "/Netflix/static/js/main.f72592e1.chunk.js"
+  },
+  {
+    "revision": "1111f8ae901435215b7d",
+    "url": "/Netflix/static/js/runtime-main.17f22a80.js"
+  }
+]);
